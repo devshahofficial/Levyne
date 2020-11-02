4242d3c2de7c5b1cb1eb64371517820024c3d2cf
Vikas Gupta <vikas@Vikass-MacBook-Air.local>
2020-11-02 21:27:35 +0530

    React Navigation Gesture Handler issue

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {StatusBar, StyleSheet, View, Platform} from 'react-native';
 import MainNavigator from './navigations/NavigatorMain';
