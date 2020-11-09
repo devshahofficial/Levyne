2c457e53e15477f0aff03f0f25b1cf6e1e1131cf
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2020-11-09 23:20:33 +0530

    icons

--- mockups/Home/Cart.js
+++ mockups/Home/Cart.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import {View, Text} from 'react-native-ui-lib';
 import {connect} from 'react-redux';
--- mockups/Home/ProductDetailsPage.js
+++ mockups/Home/ProductDetailsPage.js
@@ -1,4 +1,4 @@
-import React, { Component,Fragment } from 'react'
+import React, { Component } from 'react'
 import {ScrollView} from "react-native"
 
 import NavbarBack from "../../components/NavBarBack"
@@ -7,12 +7,12 @@ import ImageCarousel from "../../components/ImageCarousel"
 export default class ProductDetailsPage extends Component {
     render() {
         return (
-            <Fragment>
+            <>
                 <NavbarBack Title={"#RS00011"} Navigation={this.props.navigation.goBack}/>
                 <ScrollView>
                     {/* <ImageCarousel></ImageCarousel> */}
                 </ScrollView>
-            </Fragment>
+            </>
         )
     }
 }
