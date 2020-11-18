dfcaefedbe7448f67b3d08a6a3204bf8abd1e20f
Vikas <vikas.rg@somaiya.edu>
2020-11-18 16:10:20 +0530

    UI Improvement

--- API/BrandBySearch.js
+++ API/BrandBySearch.js
@@ -1,13 +1,14 @@
 import {GET} from './CustomFetch';
 
-const BrandBySearch = async (SearchKey, Page, Token, abortControllerSignal) => {
-
+const BrandBySearch = async (SearchKey, Page, OrderBy, Token, abortControllerSignal) => {
+    
     return await GET('Brand/SearchBrandProfile', {
         ReturnResponse: true,
         Token,
         QueryData: {
             SearchKey,
-            Page
+            Page,
+            OrderBy
         }
     }, abortControllerSignal)
 }
