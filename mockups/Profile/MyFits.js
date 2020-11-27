4d0365af9b7fd189f4468db26235d8595832162c
twilight-warlock <devansh.ts@somaiya.edu>
2020-11-27 14:58:25 +0530

    Initial MyFits page

--- mockups/Profile/MyFits.js
+++ mockups/Profile/MyFits.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {View, FlatList, Text} from 'react-native-ui-lib';
-import {StyleSheet} from "react-native"
+import {View,  Text} from 'react-native-ui-lib';
+import {StyleSheet, FlatList} from "react-native"
 import Input from "../../components/input"
+import Colors from '../../Style/Colors';
 import NavBarBack from '../../components/NavBarBack';
 
 export default class MyFits extends Component {
@@ -10,6 +11,7 @@ export default class MyFits extends Component {
         this.state = {
             apiTemp : [
                 {
+                    id:1,
                     name:"Arms",
                     subCategories: [
                         {
@@ -17,7 +19,7 @@ export default class MyFits extends Component {
                             val: 15
                         },
                         {
-                            name:"Triiceps",
+                            name:"Triceps",
                             val: 15
                         },
                         {
@@ -27,6 +29,7 @@ export default class MyFits extends Component {
                     ]
                 },
                 {
+                    id:2,
                     name:"Legs",
                     subCategories: [
                         {
@@ -42,85 +45,117 @@ export default class MyFits extends Component {
                             val: 20
                         },
                     ]
-                }
+                },
+                {
+                    id:3,
+                    name:"Upper Body",
+                    subCategories: [
+                        {
+                            name:"Chest",
+                            val: 32
+                        },
+                        {
+                            name:"Waist",
+                            val: 28
+                        },
+                        {
+                            name:"Hips",
+                            val: 32
+                        },
+                    ]
+                },
             ]
         }
+    }
+
+    // wordSizing(word){
+    //     return word + " ".repeat(15-word.length)
+    // }
+
+    renderItem({item}){
+        return (
+            <View style={styles.Outer}>
+                <Text style={styles.HeaderStyle}>
+                    {item.name}
+                </Text>
+                <View style={styles.InnerElementsContainer}>
+                    {item.subCategories.map(ele => (
+                        <View row>
+                            <View style={styles.InnerText}>
+                                <Text style={styles.InnerElements}>{ ele.name }</Text>
+                            </View>
+                            <View style={styles.InputBox}>
+                                <Input placeholder={`${ele.val}`} style={{width:"60%",paddingLeft:"25%",paddingTop:5}}></Input>
+                            </View>
+                        </View>
+                    ))}
+                </View>
+            </View>
+        )
     }
 
     render() {
         return(
             <>
                 <NavBarBack Navigation={this.props.navigation.goBack} Title={'My Fits and Sizes'}/>
-                <View row>
-                    {this.state.apiTemp.map(item=>{
-                        <View row>
+                <FlatList
+                    data={this.state.apiTemp}
+                    renderItem={this.renderItem}
+                    keyExtractor={item => item.id}
+                />
+                <View>
+                    {/* {this.state.apiTemp.map(item=>(
+                        <View style={styles.Outer}>
                             <Text style={styles.HeaderStyle}>
                                 {item.name}
                             </Text>
-                            <View row style={styles.InnerElementsContainer}>
-                                {item.subCategories.map(ele => {
-                                    <>
-                                        <Text style={styles.InnerElements}>{ele.name}</Text>
-                                        <Input placeholder={ele.val}></Input>
-                                    </>
-                                })}
+                            <View style={styles.InnerElementsContainer}>
+                                {item.subCategories.map(ele => (
+                                    <View row>
+                                        <View style={styles.InnerText}>
+                                            <Text style={styles.InnerElements}>{this.wordSizing( ele.name )}</Text>
+                                        </View>
+                                        <View style={styles.InputBox}>
+                                            <Input placeholder={`${ele.val}`} style={{width:"60%",paddingLeft:"25%",paddingTop:5}}></Input>
+                                        </View>
+                                    </View>
+                                ))}
                             </View>
                         </View>
-                    })}
+                    ))} */}
                 </View>
             </>
         );
     }
 }
 
-const apiTemp = [
-    {
-        name:"Arms",
-        subCategories: [
-            {
-                name:"Biceps",
-                val: 15
-            },
-            {
-                name:"Triiceps",
-                val: 15
-            },
-            {
-                name:"Wrist",
-                val: 15
-            },
-        ]
-    },
-    {
-        name:"Legs",
-        subCategories: [
-            {
-                name:"Calves",
-                val: 20
-            },
-            {
-                name:"Thighs",
-                val: 20
-            },
-            {
-                name:"Ankle",
-                val: 20
-            },
-        ]
-    }
-]
-
 const styles = StyleSheet.create({
+    Outer:{
+        flexDirection: "column",
+        paddingLeft: 20
+    },
     HeaderStyle:{
-
+        margin: 10,
+        fontSize: 18,
+        // paddingLeft: 10,
+        marginRight: "70%",
+        borderColor: "white",
+        borderBottomColor: Colors.primary,
+        borderWidth: 2
     },
     InnerElementsContainer:{
 
     },
     InnerElements:{
-
+        padding: 28,
+        fontSize: 15
     },
-    Values:{
-
+    InnerText:{
+        width: "40%",
+        marginRight:"10%"
+    },
+    InputBox: {
+        width: "60%",
+        justifyContent: "center"
     }
 })
