d376f868f6866ad063e62f0a10f05d22218fc618
Vikas <vikas.rg@somaiya.edu>
2020-11-28 15:43:06 +0530

    3D models

---
+++ assets/3DModels.js
@@ -0,0 +1,116 @@
+export default Models = {
+    Pants: {
+        ImageURL: 'https://tiimg.tistatic.com/fp/1/003/975/linen-classic-fit-black-shirt-361.jpg',
+        items: [
+            'formal_pant',
+            'side_waist_band',
+            'ankle_pant',
+            'straight_pant',
+            'narrow_pant',
+            'tapered_pant'
+        ]
+    },
+    Blouse: {
+        ImageURL: 'https://tiimg.tistatic.com/fp/1/003/975/linen-classic-fit-black-shirt-361.jpg',
+        items: [
+            'square_neck_flounce_sleeve',
+            'pleated_neck_pleated_sleeve',
+            'boat_neck_3-4th_sleeves',
+            'flare_sleeve',
+            'frilled_neck',
+            'high_neck_',
+            'cleric_neck',
+            'W_neck_scalloped_sleeve',
+            'broken_moon_neck_cap_sleeve',
+            'asymmetric_neck_asymmetric_sleeve',
+            'strip_neck',
+            'chinese_collar_keyhole_sleeve',
+            'jacket_style',
+            'diamond_neck_puff_sleeve',
+            'tulip_sleeve',
+            'halter_top',
+            'cold_shoulders',
+            'V_cross_neck',
+            'layer_sleeve',
+            'boat_neck_frilled_sleeves',
+            'cross_neck',
+            'chelsea_neck_full_sleeves',
+            'V_neck_full_cuff_sleeve',
+            'scalloped_neck_tulip_sleeve',
+            'keyhole_neck_ruffle_sleeve',
+            'sweatheart_neck_puff_sleeve',
+            'oblong_choli',
+            'slit_neck_leg-o-mutton_sleeve',
+            'scooped_neck_bell_sleeves',
+            'round_neck_layered_sleeve',
+            'innovative_V_NECK',
+            'beatle_neck_elbow_sleeve'
+        ]
+    },
+    Kurti: {
+        ImageURL: 'https://tiimg.tistatic.com/fp/1/003/975/linen-classic-fit-black-shirt-361.jpg',
+        items: [
+            'scalloped__kurti',
+            'jacket_style_straight_cut_kurti',
+            'high-low_kurti',
+            'peplum_jacket_style_kurti',
+            'vertical_silhouette_with_puff_sleeve_',
+            'half_pleated_kurti',
+            'decollete_neck_kurti',
+            'triangle_silhouette_',
+            'asymmetrical_kurti',
+            'balloon_sleeves_kurti',
+            'pleated__kurti',
+            'shawl_neck_straight_fit_kurti'
+        ]
+    },
+    Blazer: {
+        ImageURL: 'https://tiimg.tistatic.com/fp/1/003/975/linen-classic-fit-black-shirt-361.jpg',
+        items: [
+            'peak_blazer',
+            'Double_breasted_peak_lapel_blazer',
+            'shawl_flap_blazer',
+            'shawl_with_a_flap_blazer',
+            'notch_blazer',
+            'Double_breasted_shawl_blazer',
+            'Double_breasted_notch_lapel_blazer'
+        ]
+    },
+    Vest: {
+        ImageURL: 'https://tiimg.tistatic.com/fp/1/003/975/linen-classic-fit-black-shirt-361.jpg',
+        items: [
+            'double_breasted_shawl_lapel_vest',
+            'four_button_vest',
+            'double_breasted_patch_lapel_vest',
+            'asymmetrical_vest',
+            'U-neck_vest',
+            'U-neck_notch_lapel_vest',
+            'double_breasted_vest',
+            'basic_notch_lapel_vest',
+            'basic_peak_lapel_vest',
+            'two_button_vest',
+            'three_button_vest',
+            'double_breasted_notch_lapel_vest',
+            'U-neck_peak_lapel_vest'
+        ]
+    },
+    Shirt: {
+        ImageURL: 'https://tiimg.tistatic.com/fp/1/003/975/linen-classic-fit-black-shirt-361.jpg',
+        items: [
+            'spread_eagle_collar_2_button_cuff',
+            'double_collar_round_cuff_',
+            'prince_charlie_collar_ankle_cut_cuff',
+            'club_collar_3_button_cuff',
+            'basic_shirt',
+            'polo_collar_double_round_cuff',
+            'concealed_polo_collar_',
+            'wing_collar_angle_cuff',
+            'madmen_collar_double_cuff',
+            'prepster_collar_square_cuff',
+            'hipster_collar_triangular_cuff',
+            'bandgala_collar_round_cuff',
+            'evil_pandit_collar',
+            'ozwald_collar_'
+        ]
+    }
+}
--- mockups/Customize.js
+++ mockups/Customize.js
@@ -3,6 +3,7 @@ import {StyleSheet, Dimensions, FlatList} from 'react-native';
 import {View, Text, TouchableOpacity, Image} from 'react-native-ui-lib';
 import {connect} from 'react-redux';
 import TextNavBar from '../components/TextNavBar';
+import Models from '../assets/3DModels';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -34,43 +35,21 @@ const ProductsData = [
     }
 ];
 
-const threeD = [
-    {
-        id: "guwiqugd",
-        Title: "Shirt",
-        Image: "https://tiimg.tistatic.com/fp/1/003/975/linen-classic-fit-black-shirt-361.jpg"
-    },
-    {
-        id: "oqhguwiqugd",
-        Title: "Pant",
-        Image: "https://www.ericmusgrave.co.uk/wp-content/uploads/MM-5.jpg"
-    },
-    {
-        id: "dajvka",
-        Title: "Choli",
-        Image: "https://singaporebrides.com/articles/wp-content/uploads/2017/05/Bespoke-Mens-Tailors-for-Weddings-in-Singapore-Dorcas-Stitch.jpg"
-    }
-];
-
 class Customize extends React.Component {
 
-    state = {
-        imgWidth: 0,
-        imgHeight: 0,
-    }
+    imgWidth = Dimensions.get('window').width - 40;
+    imgHeight = 80*(Dimensions.get('window').width - 40)/335;
 
-    componentDidMount() {
+    ModelKeys = Object.keys(Models);
 
-        Image.getSize("https://d364lz1eqz350v.cloudfront.net/MensWear.png", (width, height) => {
-            // calculate image width and height
-            const screenWidth = Dimensions.get('window').width-40
-            const scaleFactor = width / screenWidth
-            const imageHeight = height / scaleFactor
-            this.setState({imgWidth: screenWidth, imgHeight: imageHeight})
-        })
-    }
-
-
+    headerContainerRender = ({ item }) => (
+        <TouchableOpacity style={styles.Category} center>
+            <View>
+                <Image source={{uri: Models[item].ImageURL}} style={styles.ImageUpper}/>
+            </View>
+            <Text h3 secondary marginT-10>{item}</Text>
+        </TouchableOpacity>
+    )
 
     headerContainer = () => {
         return(
@@ -78,18 +57,11 @@ class Customize extends React.Component {
                 <Text marginL-5 marginV-10 hb1>3D by Levyne</Text>
                 <View center>
                     <FlatList
-                        data={threeD}
+                        data={this.ModelKeys}
                         horizontal={true}
                         showsHorizontalScrollIndicator={false}
-                        renderItem={
-                            ({ item }) =>
-                                <TouchableOpacity style={styles.Category} center>
-                                    <View>
-                                        <Image source={{uri:item.Image}} style={styles.ImageUpper}/>
-                                    </View>
-                                    <Text h3 secondary marginT-10>{item.Title}</Text>
-                                </TouchableOpacity>
-                        }
+                        renderItem = {this.headerContainerRender}
+                        keyExtractor={(item) => item}
                     />
                 </View>
                 <Text marginL-5 marginV-10 hb1>Designs by Levyne</Text>
@@ -97,8 +69,17 @@ class Customize extends React.Component {
         )
     }
 
+    renderItem = ({ item }) => (
+        <TouchableOpacity centerH style={[styles.Container,{width: this.imgWidth, height: this.imgHeight}]}>
+            <Image
+                source={{uri:item.Image}}
+                style={{ width: this.imgWidth, height: this.imgHeight }}
+            />
+        </TouchableOpacity>
+    )
+
+
     render() {
-        const {imgWidth, imgHeight} = this.state;
         return (
             <>
                 <TextNavBar Title={'Customize on Levyne'}/>
@@ -107,17 +88,8 @@ class Customize extends React.Component {
                         data={ProductsData}
                         ListHeaderComponent={this.headerContainer}
                         showsVerticalScrollIndicator={false}
-                        renderItem={
-                            ({ item }) =>
-                                <TouchableOpacity
-                                    style={[styles.Container,{width: imgWidth, height: imgHeight}]}
-                                >
-                                    <Image
-                                        source={{uri:item.Image}}
-                                        style={{ width: imgWidth, height: imgHeight }}
-                                    />
-                                </TouchableOpacity>
-                        }
+                        renderItem={this.renderItem}
+                        keyExtractor={(item) => item.id}
                     />
                 </View>
             </>
