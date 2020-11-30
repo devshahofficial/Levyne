93258b29c2a6841f829955f6ef0b647d5af13b9f
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2020-11-30 13:05:05 +0530

    Unlogged screen.js

--- components/UnLoggedScreen.js
+++ components/UnLoggedScreen.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import {View, Text} from 'react-native-ui-lib';
+import {View, Button, Colors} from 'react-native-ui-lib';
+import LoginSVG from "../assets/images/AppImages/Login.svg";
+
 export default class UnLoggedScreen extends React.PureComponent {
 
     render() {
         return (
-            <View flex center>
-                <Text>Complete Login</Text>
-            </View>
+            <>
+                <View flex center>
+                    <LoginSVG width={'90%'}/>
+                </View>
+                <Button
+                    hb1 label={'Login'}
+                    style={{
+                        borderRadius:0,
+                        borderWidth:1,
+                        borderColor: Colors.primary
+                    }}
+                />
+            </>
+
         );
     }
 }
