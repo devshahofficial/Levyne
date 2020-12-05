41c0563d301509494aa3095e505bee0aa24ef688
Vikas <vikas.rg@somaiya.edu>
2020-12-05 23:19:41 +0530

    order complete

--- API/Checkout.js
+++ API/Checkout.js
@@ -1,14 +1,15 @@
 import { POST } from "./CustomFetch";
 
-const Checkout = async (Address, PinCode, Comment, BrandID, Token, abortControllerSignal) => {
-    return await POST("Fabrics/AddToWishList", {
+const Checkout = async (Address, PinCode, Comment, BucketID, Token, abortControllerSignal) => {
+    console.log(Address, PinCode, Comment, BucketID, Token);
+    return await POST("Orders/Checkout", {
         ReturnResponse: true,
         Token,
         Body: {
             Address,
             PinCode,
             Comment,
-            BrandID
+            BucketID
         }
     }, abortControllerSignal)
 }
