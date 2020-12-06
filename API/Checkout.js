cf4b4316807174aeb1cc272f7248968579518d7f
Vikas <vikas.rg@somaiya.edu>
2020-12-06 15:48:43 +0530

    Checkout chat

--- API/Checkout.js
+++ API/Checkout.js
@@ -1,7 +1,6 @@
 import { POST } from "./CustomFetch";
 
 const Checkout = async (Address, PinCode, Comment, BucketID, Token, abortControllerSignal) => {
-    console.log(Address, PinCode, Comment, BucketID, Token);
     return await POST("Orders/Checkout", {
         ReturnResponse: true,
         Token,
