7040d2574bdd80be34085b8c0c577ce6acae8e78
Vikas <vikas.rg@somaiya.edu>
2020-12-15 11:56:08 +0530

    My Fits

--- assets/FitsMale.js
+++ assets/FitsMale.js
@@ -1,43 +1,42 @@
-export default FitsMale = {
-	"Upper Body": {
-		"items": [
-			"Neck Round",
-			"Chest Round",
-			"Shoulder to Chest Length",
-			"Shoulder to Waist Length"
-		],
-		"Image": "https://images.unsplash.com/photo-1562373353-7db88744f982?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGRyZXNzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"
-	},
-	"Waist": {
-		"items": [
-			"Stomach Round",
-			"Waist Round",
-			"Hip Round",
-			"Waist to Hip Length",
-			"Waist to Below Croch Length",
-			"Waist to Above Knee",
-			"Waist to Ankle",
-			"Waist to Floor",
-			"Stomach"
-		],
-		"Image": "https://images.unsplash.com/photo-1530893608544-cd10fda1ac14?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8ZHJlc3N8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60"
-	},
-	"Arms": {
-		"items": [
-			"Armhole Round",
-			"Cap Sleeves Round (biceps)",
-			"Half Sleeves Round",
-			"1/2 Sleeve Length (Above Elbow)",
-			"Full Sleeve Length"
-		],
-		"Image": "https://images.unsplash.com/photo-1599255260684-682356f09ab1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTh8fGRyZXNzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"
-	},
-	"Legs": {
-		"items": [
-			"Single Thigh Round",
-			"Above Knee Round",
-			"Ankle Round"
-		],
-		"Image": "https://images.unsplash.com/photo-1562373353-7db88744f982?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGRyZXNzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"
-	}
-}
+export default FitsMale = [{
+	Title: "Upper Body",
+	data: [
+		"Neck Round",
+		"Chest Round",
+		"Shoulder to Chest Length",
+		"Shoulder to Waist Length"
+	],
+	Image: "https://images.unsplash.com/photo-1562373353-7db88744f982?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGRyZXNzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"
+}, {
+	Title: "Waist",
+	data: [
+		"Stomach Round",
+		"Waist Round",
+		"Hip Round",
+		"Waist to Hip Length",
+		"Waist to Below Croch Length",
+		"Waist to Above Knee",
+		"Waist to Ankle",
+		"Waist to Floor",
+		"Stomach"
+	],
+	Image: "https://images.unsplash.com/photo-1530893608544-cd10fda1ac14?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8OHx8ZHJlc3N8ZW58MHx8MHw%3D&auto=format&fit=crop&w=500&q=60"
+}, {
+	Title: "Arms",
+	data: [
+		"Armhole Round",
+		"Cap Sleeves Round (biceps)",
+		"Half Sleeves Round",
+		"1/2 Sleeve Length (Above Elbow)",
+		"Full Sleeve Length"
+	],
+	Image: "https://images.unsplash.com/photo-1599255260684-682356f09ab1?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NTh8fGRyZXNzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"
+}, {
+	Title: "Legs",
+	data: [
+		"Single Thigh Round",
+		"Above Knee Round",
+		"Ankle Round"
+	],
+	Image: "https://images.unsplash.com/photo-1562373353-7db88744f982?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGRyZXNzfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"
+}]
