baff1cfac680dc087fd0dc3720766d366ff701d0
Vikas <vikas.rg@somaiya.edu>
2020-12-20 22:32:07 +0530

    Removed unused part

--- components/CstmShadowView.js
+++ components/CstmShadowView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Platform} from 'react-native';
+import {StyleSheet} from 'react-native';
 import ShadowView from 'react-native-simple-shadow-view';
 import Colors from "../Style/Colors";
 
