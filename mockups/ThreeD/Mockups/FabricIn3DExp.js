869cbcd77d7a5b14ec88473c11ac944b2d6ba26d
Vikas <vikas.rg@somaiya.edu>
2020-12-30 15:20:29 +0530

    FabricIn3D

--- mockups/ThreeD/Mockups/FabricIn3DExp.js
+++ mockups/ThreeD/Mockups/FabricIn3DExp.js
@@ -1,77 +1,67 @@
 import React, { Component } from 'react';
-import {StyleSheet, FlatList, Linking, SafeAreaView} from 'react-native';
-import {Colors, Text, TouchableOpacity, View} from "react-native-ui-lib";
-import UpperComponent from '../Components/UpperComponent'
+import {SafeAreaView} from 'react-native';
+import {Colors, Text, TouchableOpacity} from "react-native-ui-lib";
 import NavBarBack from "../../../components/NavBarBack";
 import WebView from "react-native-webview";
-import Models from '../../../assets/3DModels';
 import {CallIcon} from "../../../Icons/CallIcon";
+import ImagePicker from 'react-native-image-crop-picker';
 
 export default class NewScreen extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            upperSelected: 0,
-            lowerSelected: 0
-        }
-
-        this.Category = this.props.route.params.Category
-
-        if(this.Category) {
-            this.URL3DModelBase = `https://3d.levyne.com/${this.Category}/`;
-            this.Model = Models[this.Category];
-        } else {
-            this.goBack();
-        }
-    }
-
-
-    onUpperPressed = (selectedIndex) => {
-        this.setState({ upperSelected: selectedIndex })
-    }
-
-    onLowerPressed = (selectedIndex) => {
-        this.setState({ lowerSelected: selectedIndex })
-    }
+    URL3DModelBase = `https://3d.levyne.com`;
 
     goBack = () => {
         this.props.navigation.goBack();
     }
 
-    renderItem = ({ item, index }) => (
-        <UpperComponent
-            item={item}
-            index={index}
-            selected={this.state.upperSelected}
-            onUpperPressed={this.onUpperPressed}
-        />
-    )
+    RNWebview = React.createRef();
 
+    state = {
+        injectedJavaScript : ''
+    }
+
+    FabricImages = [];
+
+    TouchableOpacityOnPress = () => {
+
+        /*
+        
+        */
+
+        ImagePicker.openPicker({
+            writeTempFile: false,
+            width: 1024,
+            height: 1024,
+            includeBase64: true,
+            forceJpg: true
+        }).then(resp => {
+            this.FabricImages.push(`data:${resp.mime};base64,${resp.data}`);
+            this.setState({
+                injectedJavaScript: 'window.FabricImages = ' + JSON.stringify(this.FabricImages)
+            })
+    
+            this.RNWebview.current.reload();
+        })
+    }
 
     render() {
         return (
             <>
                 <NavBarBack Title={"Design 3D here!"} Navigation={this.goBack} />
                 <SafeAreaView style={{flex: 1}}>
-                    <View>
-                        <FlatList
-                            showsHorizontalScrollIndicator={false}
-                            style={styles.FlatList}
-                            data={this.Model.items}
-                            horizontal={true}
-                            renderItem={this.renderItem}
-                            keyExtractor={item => item}
-                        />
-                    </View>
 
-                    <WebView source={{ uri: this.URL3DModelBase + this.Model.items[this.state.upperSelected] }}/>
+                    <WebView
+                        source={{ uri: this.URL3DModelBase }}
+                        injectedJavaScriptBeforeContentLoaded={this.state.injectedJavaScript}
+                        onMessage={console.log}
+                        ref={this.RNWebview}
+                    />
                     <TouchableOpacity
                         style={{height:50, backgroundColor: Colors.primary}} center
-                        onPress={() => Linking.openURL('tel:+91 9819 077182')} row
+                        onPress={this.TouchableOpacityOnPress} row
                     >
                         <CallIcon Size={18} Color={Colors.white}/>
-                        <Text h1 white marginL-20>Call us for product enquire</Text>
+                        <Text h1 white marginL-20>Add Fabric Images</Text>
                     </TouchableOpacity>
                 </SafeAreaView>
             </>
@@ -80,10 +70,4 @@ export default class NewScreen extends Component {
     }
 }
 
-const styles = StyleSheet.create({
-    FlatList: {
-        marginTop: 10,
-        paddingLeft: 20,
-    },
-})
 
