ded2d26ec269dc208744831b2a608e6a0b5c0239
Vikas <vikas.rg@somaiya.edu>
2021-01-07 13:11:36 +0530

    Removed Deprecated Packages

--- API/EditProfile.js
+++ API/EditProfile.js
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { POST } from './CustomFetch';
 
 const EditProfile = async (Name, Email, Address, Gender, PinCode, Token) => {
