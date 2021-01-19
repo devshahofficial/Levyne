023850f11da50e73f302020d09ae0100c964a94a
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2021-01-19 16:27:33 +0530

    All changes

--- components/Square.js
+++ components/Square.js
@@ -11,18 +11,18 @@ export default class Square extends React.Component {
             <View>
                 <View row>
                     <TouchableOpacity flex style={styles.msgLeft}>
-                        <Image source={{uri: this.props.Image}} style={styles.img} />
+                        <Image source={{uri: "https://i.ibb.co/h7LXd3D/Group-4177.png"}} style={styles.img} />
                     </TouchableOpacity>
                     <TouchableOpacity flex style={styles.msgRight}>
-                        <Image source={{uri: this.props.Image}} style={styles.img} />
+                        <Image source={{uri: "https://i.ibb.co/b1HxvjL/Group-4178.png"}} style={styles.img} />
                     </TouchableOpacity>
                 </View>
                 <View row>
                     <TouchableOpacity flex style={styles.msgLeft}>
-                        <Image source={{uri: this.props.Image}} style={styles.img} />
+                        <Image source={{uri: "https://i.ibb.co/w4YN5Sw/Group-4176.png"}} style={styles.img} />
                     </TouchableOpacity>
                     <TouchableOpacity flex style={styles.msgRight}>
-                        <Image source={{uri: this.props.Image}} style={styles.img} />
+                        <Image source={{uri: "https://i.ibb.co/r7zSrJ3/Group-4179.png"}} style={styles.img} />
                     </TouchableOpacity>
                 </View>
             </View>
