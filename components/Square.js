84fb6eda5ea9a11911fb713e60508c5a722c8ff3
Vikas <vikas.rg@somaiya.edu>
2021-01-19 20:19:48 +0530

    Navigation

--- components/Square.js
+++ components/Square.js
@@ -10,19 +10,19 @@ export default class Square extends React.Component {
         return (
             <View>
                 <View row>
-                    <TouchableOpacity flex style={styles.msgLeft}>
-                        <Image source={{uri: "https://i.ibb.co/h7LXd3D/Group-4177.png"}} style={styles.img} />
+                    <TouchableOpacity onPress={() => NavigateThreeD(1)} flex style={styles.msgLeft}>
+                        <Image source={{uri: "http://d32kprqn8e36ns.cloudfront.net/TrousersHPDiscount.webp"}} style={styles.img} />
                     </TouchableOpacity>
-                    <TouchableOpacity flex style={styles.msgRight}>
-                        <Image source={{uri: "https://i.ibb.co/b1HxvjL/Group-4178.png"}} style={styles.img} />
+                    <TouchableOpacity onPress={() => NavigateThreeD(0)} flex style={styles.msgRight}>
+                        <Image source={{uri: "http://d32kprqn8e36ns.cloudfront.net/ShirtHPDiscount.webp"}} style={styles.img} />
                     </TouchableOpacity>
                 </View>
                 <View row>
-                    <TouchableOpacity flex style={styles.msgLeft}>
-                        <Image source={{uri: "https://i.ibb.co/w4YN5Sw/Group-4176.png"}} style={styles.img} />
+                    <TouchableOpacity onPress={() => NavigateThreeD(3)} flex style={styles.msgLeft}>
+                        <Image source={{uri: "http://d32kprqn8e36ns.cloudfront.net/KurtiHPDiscount.webp"}} style={styles.img} />
                     </TouchableOpacity>
-                    <TouchableOpacity flex style={styles.msgRight}>
-                        <Image source={{uri: "https://i.ibb.co/r7zSrJ3/Group-4179.png"}} style={styles.img} />
+                    <TouchableOpacity onPress={() => NavigateThreeD(4)} flex style={styles.msgRight}>
+                        <Image source={{uri: "http://d32kprqn8e36ns.cloudfront.net/CholiHPDiscount.webp"}} style={styles.img} />
                     </TouchableOpacity>
                 </View>
             </View>
