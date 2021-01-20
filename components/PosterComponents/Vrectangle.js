865acb1f6fab944bac6d603042236515a76056d1
Dev Shailesh Shah <49101333+devshahofficial@users.noreply.github.com>
2021-01-20 16:38:07 +0530

    All changes

---
+++ components/PosterComponents/Vrectangle.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {TouchableOpacity, View} from 'react-native-ui-lib';
+import {StyleSheet, Dimensions, Image} from 'react-native';
+
+const {height} = Dimensions.get('window');
+
+
+export default class Vrectangle extends React.Component {
+    render() {
+        return (
+            <View row>
+                <TouchableOpacity onPress={this.props.onPress} flex style={styles.msg}>
+                    <Image source={{uri: "https://i.ibb.co/KLVCrJm/Group-4188.jpg"}} style={styles.img} />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this.props.onPress} flex style={styles.msg}>
+                    <Image source={{uri: "https://i.ibb.co/5sZrzP0/Group-4189.jpg"}} style={styles.img} />
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    img: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    msg: {
+        marginTop: 10,
+        height: height * 0.30,
+        borderRadius: 10,
+        margin: 10,
+    },
+});
