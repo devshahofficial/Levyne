71d6d59a33ad2bed3711b34f1742918e2043ec2f
Vikas <vikas.rg@somaiya.edu>
2021-02-04 20:26:20 +0530

    Product Review Styling

--- components/ReviewForProducts.js
+++ components/ReviewForProducts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Avatar, AvatarHelper } from 'react-native-ui-lib';
+import { Text, View, Avatar, AvatarHelper, Colors } from 'react-native-ui-lib';
 import { StyleSheet } from 'react-native';
 import StarIconsComponent from './StarIconsComponent';
 import { DescriptionCard } from './ReadMore';
@@ -17,17 +17,17 @@ export default class ReviewForProducts extends React.PureComponent {
 					const Initials = AvatarHelper.getInitials(item.CustomerName);
 					return (
 						<View key={index.toString()} style={styles.container}>
-							<View style={styles.subContainer}>
+							<View row>
 								<Avatar label={Initials} style={styles.img} />
 								<View marginL-10 marginT-5>
 									<Text hb2>{item.CustomerName}</Text>
-									<Text hb2>{item.HoursAgo} Hours Ago</Text>
+									<Text h3>{item.HoursAgo} Hours Ago</Text>
 								</View>
-								<View flex margin-10 style={styles.txt}>
+								<View flex marginV-10 row right>
 									<StarIconsComponent BrandRating={item.Rating}></StarIconsComponent>
 								</View>
 							</View>
-							<View marginV-10>
+							<View marginV-10 marginL-5>
 								<DescriptionCard CompleteDescription={item.Review} />
 							</View>
 						</View>
@@ -40,19 +40,14 @@ export default class ReviewForProducts extends React.PureComponent {
 
 const styles = StyleSheet.create({
 	container: {
-		height: 'auto',
 		borderRadius: 10,
-	},
-	subContainer: {
-		flexDirection: 'row',
+		borderBottomColor: Colors.shadow,
+		borderBottomWidth: 1,
+		marginBottom: 10
 	},
 	img: {
 		width: 60,
 		height: 60,
 		borderRadius: 100,
-	},
-	txt: {
-		flexDirection: 'row',
-		justifyContent: 'flex-end',
 	},
 });
