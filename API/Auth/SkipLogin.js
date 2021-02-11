cc473c5bd6876671afb7084bead2c0650cc7c518
Vikas <vikas.rg@somaiya.edu>
2021-02-11 11:02:43 +0530

    Skip Login

--- API/Auth/SkipLogin.js
+++ API/Auth/SkipLogin.js
@@ -9,15 +9,26 @@ import Constants from '../../assets/constants';
 const SkipLogin = async (setAuth, setProfile, ProfileStatus) => {
     setAuth({SkipLogin: true});
     setProfile({ProfileStatus: ProfileStatus});
-    // @ts-ignore
-    fetch(global.BaseURL + 'SkipLogin', {
-        method: 'POST',
-        body: JSON.stringify({DeviceID: Constants.DeviceID})
-    }).catch(console.log);
+
+    const LastUpdated = await AsyncStorage.getItem('LastUpdated');
+
+    if(!(LastUpdated && (new Date().toISOString().substring(0,13)) === LastUpdated)) {
+        // @ts-ignore
+        fetch(global.BaseURL + 'SkipLogin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({DeviceID: Constants.DeviceID})
+        }).then(async resp => {
+            console.log(await resp.text());
+        }).catch(console.log);
+    }
     
     return await AsyncStorage.multiSet([
         ['SkipLogin', '1'],
         ['ProfileStatus', '0'],
+        ['LastUpdated', new Date().toISOString().substring(0,13)]
     ]);
 }
 export default SkipLogin;
