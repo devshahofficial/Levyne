cd39f26b0f03edaac91cbefc0bcb33b88c794f69
Vikas <vikas.rg@somaiya.edu>
2021-03-18 13:27:06 +0530

    Prettier

--- API/CustomFetch.ts
+++ API/CustomFetch.ts
@@ -1,71 +1,93 @@
-const encodeQueryData = function (obj: { [key: string]: any; }, prefix: string | null): string {
+const encodeQueryData = function (
+    obj: {[key: string]: any} | undefined,
+    prefix: string | null,
+): string {
     let str = [],
         p;
-    for (p in obj) {
-        if (obj.hasOwnProperty(p)) {
-            let k = prefix ? prefix + "[" + p + "]" : p,
-                v = obj[p];
-            str.push(
-                v !== null && typeof v === "object"
-                    ? encodeQueryData(v, k)
-                    : encodeURIComponent(k) + "=" + encodeURIComponent(v)
-            );
+    if (obj) {
+        for (p in obj) {
+            if (obj.hasOwnProperty(p)) {
+                let k = prefix ? prefix + '[' + p + ']' : p,
+                    v = obj[p];
+                str.push(
+                    v !== null && typeof v === 'object'
+                        ? encodeQueryData(v, k)
+                        : encodeURIComponent(k) + '=' + encodeURIComponent(v),
+                );
+            }
         }
     }
-    return str.join("&");
+    return str.join('&');
 };
 
-
-export const POST = async (URL: string, Config: { ReturnResponse?: boolean, ThrowError?: boolean, Token?:string, Body?: { [key: string]: any; } }, abortControllerSignal: AbortSignal | undefined = undefined) : Promise<any> => {
+export const POST = async (
+    URL: string,
+    Config: {
+        ReturnResponse?: boolean;
+        ThrowError?: boolean;
+        Token?: string;
+        Body?: {[key: string]: any};
+    },
+    abortControllerSignal: AbortSignal | undefined = undefined,
+): Promise<any> => {
     const resp = await fetch(global.BaseURL + URL, {
         method: 'POST',
         signal: abortControllerSignal,
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-            ...(Config.Token) && { 'Authorization': Config.Token }
+            ...(Config.Token && {Authorization: Config.Token}),
         },
-        ...(Config.Body) && { body: JSON.stringify(Config.Body) }
+        ...(Config.Body && {body: JSON.stringify(Config.Body)}),
     });
-    if (resp.status != 200) {
+    if (resp.status !== 200) {
         if (Config.ThrowError) {
             throw await resp.text();
         }
         throw resp.status;
-    }
-    else {
+    } else {
         if (Config.ReturnResponse) {
             return await resp.json();
         }
         return;
     }
-}
+};
 
-export const GET = async (URL: string, Config: { ReturnResponse?: boolean, ThrowError?: boolean, Token?:string, QueryData?: { [key: string]: any; } }, abortControllerSignal: AbortSignal | undefined = undefined) : Promise<any> => {
-
+export const GET = async (
+    URL: string,
+    Config: {
+        ReturnResponse?: boolean;
+        ThrowError?: boolean;
+        Token?: string;
+        QueryData?: {[key: string]: any};
+    },
+    abortControllerSignal: AbortSignal | undefined = undefined,
+): Promise<any> => {
     // @ts-ignore
-    const resp = await fetch(global.BaseURL + URL + '?' + encodeQueryData(Config.QueryData, null), {
-        method: 'GET',
-        signal: abortControllerSignal,
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            ...(Config.Token) && { 'Authorization': Config.Token }
+    const resp = await fetch(
+        global.BaseURL + URL + '?' + encodeQueryData(Config.QueryData, null),
+        {
+            method: 'GET',
+            signal: abortControllerSignal,
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                ...(Config.Token && {Authorization: Config.Token}),
+            },
         },
-    });
-    if (resp.status != 200) {
-        if(Config.ThrowError) {
+    );
+    if (resp.status !== 200) {
+        if (Config.ThrowError) {
             throw {
                 Error: await resp.json(),
-                Status: resp.status
-            }
+                Status: resp.status,
+            };
         }
         throw resp.status;
-    }
-    else {
+    } else {
         if (Config.ReturnResponse) {
             return await resp.json();
         }
         return;
     }
-}
+};
