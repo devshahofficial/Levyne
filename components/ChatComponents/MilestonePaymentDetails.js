48e375fcd61ad45f54144174508076da1b4ebc1f
Vikas <vikas.rg@somaiya.edu>
2021-04-12 13:41:19 +0530

    Milestone Payment done

--- components/ChatComponents/MilestonePaymentDetails.js
+++ components/ChatComponents/MilestonePaymentDetails.js
@@ -2,13 +2,15 @@ import React from 'react';
 import CstmShadowView from '../../components/CstmShadowView';
 import {Text, TouchableOpacity} from 'react-native-ui-lib';
 
-const MilestonePaymentDetails = ({Price, Note, isPaymentDone, onPress}) => (
+const MilestonePaymentDetails = ({Price, Note, PaymentTimestamp, onPress}) => (
     <CstmShadowView style={{height:"auto", padding:15, borderRadius:5, margin: 20,}}>
         <Text hb1>₹{Price}</Text>
         <Text h2 secondary>{Note}</Text>
         {
-            isPaymentDone ?
-                <></>
+            PaymentTimestamp ?
+                <TouchableOpacity onPress={onPress} marginT-15 styles={{justifyContent:"flex-start"}}>
+                    <Text primary>Paid at {new Date(PaymentTimestamp).toDateString()}</Text>
+                </TouchableOpacity>
                 :
                 <TouchableOpacity onPress={onPress} marginT-15 styles={{justifyContent:"flex-start"}}>
                     <Text primary>Secure Pay</Text>
